e26599d3bd5c1812037992579a4fe8da6677ef16
maxlinke <maxlinke@noreply.github.com>
2018-07-08 16:47:59 +0200

    Primitive Implementation of Snapping things to the spline
    > Uses a global "resolution" and cascades. I'm going to try to replace
    it by going over each segment.

--- Assets/Objects/Track Trigger/TrackTrigger.cs
+++ Assets/Objects/Track Trigger/TrackTrigger.cs
@@ -2,12 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using BezierSolution;
 
 public class TrackTrigger : MonoBehaviour {
 
 	[SerializeField] UnityEvent action;
+	[SerializeField] BezierSpline curve;
+	[SerializeField] [Range(0f,1f)] float positionOnCurve;
 
-	void OnTriggerEnter(Collider otherCollider){
+	const int testResolution = 10000;
+	const int cascades = 3;
+
+	public void ApplyPosition () {
+		transform.position = curve.GetPoint(positionOnCurve);
+	}
+
+	public void SnapToCurve () {
+		float closestPos = 0;
+		float closestSqrDist = Mathf.Infinity;
+		float start = 0f;
+		float end = 1f;
+		for(int i=0; i<cascades; i++){
+			for(int j=0; j<=testResolution; j++){
+				float frac = ((float)j / testResolution);
+				float pos = ((1f - frac) * start) + (frac * end);
+				float sqrDist = (transform.position - curve.GetPoint(pos)).sqrMagnitude;
+				if(sqrDist < closestSqrDist){
+					closestSqrDist = sqrDist;
+					closestPos = pos;
+				}
+			}
+			start = Mathf.Clamp01(closestPos - (1f / testResolution));
+			end = Mathf.Clamp01(closestPos + (1f / testResolution));
+		}
+		positionOnCurve = closestPos;
+		ApplyPosition();
+	}
+
+	public void DebugThing(){
+		for(int i=0; i<curve.Count; i++){
+			float frac = ((float)i / (curve.Count - 1));
+			Debug.DrawRay(curve.GetPoint(frac), Vector3.up * 100, Color.red, 1f, false);
+		}
+	}
+
+	void OnTriggerEnter (Collider otherCollider) {
 		if(otherCollider.CompareTag("TrackFollower")){
 			action.Invoke();
 		}else{
---
+++ Assets/Objects/Track Trigger/TrackTriggerEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TrackTrigger))]
+public class TrackTriggerEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		DrawDefaultInspector();
+		TrackTrigger tt = target as TrackTrigger;
+		if(GUILayout.Button("Apply Position")){
+			tt.ApplyPosition();
+		}
+		if(GUILayout.Button("Snap To Curve")){
+			tt.SnapToCurve();
+		}
+		if(GUILayout.Button("Debug Thing")){
+			tt.DebugThing();
+		}
+	}
+
+}
